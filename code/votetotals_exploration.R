a4057e37adf62a0e03a6f654d29871672fab70cf
juanbrasdefer <juan.brasdefer@sciencespo.fr>
2024-10-24 18:25:20 -0400

    Cluster NetMargin

---
+++ code/votetotals_exploration.R
@@ -0,0 +1,67 @@
+# libraries -------------------------------------------------------------------
+
+# loading
+library(tidyverse)
+library(here)
+
+# set directory
+here::i_am("code/votetotals_exploration.R")
+
+countypres_raw <- read_csv(here("data/countypres_2000-2020.csv"))
+
+# USING FIPS ----------------------------------------------------------------
+
+voting_as_wide <- countypres_raw %>%
+  pivot_wider(names_from = party, 
+              values_from = candidatevotes)
+
+voting_2020_wide <- voting_as_wide %>%
+  filter(year == 2020) %>%
+  group_by(state,
+           state_po,
+           county_name,
+           county_fips,
+           totalvotes) %>%
+  summarise(aggvotes_D = sum(DEMOCRAT, na.rm = TRUE),
+            aggvotes_R = sum(REPUBLICAN, na.rm = TRUE),
+            aggvotes_G = sum(GREEN, na.rm = TRUE),
+            aggvotes_L = sum(LIBERTARIAN, na.rm = TRUE),
+            aggvotes_O = sum(OTHER, na.rm = TRUE))
+  
+
+minvotes <- min(voting_2020_wide$totalvotes)
+maxvotes <- max(voting_2020_wide$totalvotes)
+
+voting_DR_margins <- voting_2020_wide %>%
+  mutate(
+    proportion_D = aggvotes_D / totalvotes,  # Democratic proportion
+    proportion_R = aggvotes_R / totalvotes,  # Republican proportion
+    net_DR_margin = proportion_D - proportion_R) %>% # Net margin: positive = Dem, negative = Rep
+  mutate(
+    norm_votes = (totalvotes - minvotes) / 
+      (maxvotes - minvotes),
+    lognorm_votes = log(totalvotes + 1))
+
+
+`
+
+
+voting_DR_margins %>%
+ggplot(aes(x = lognorm_votes, 
+           y = net_DR_margin 
+           #color = net_DR_margin
+           )) +
+  geom_point(alpha = 0.6,
+             color = "blue") +  # Add some transparency to avoid overplotting
+  labs(title = "Vote Margin v Totals", 
+       x = "total votes in county", 
+       y = "net margin") +
+  theme_minimal()  
+
+
+
+
+
+
+
+
